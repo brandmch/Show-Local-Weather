8cab98e59dee10539f5b3cebbd37dcb0af668596
brandmch <101467867+brandmch@users.noreply.github.com>
2023-02-01 10:48:47 -0500

    button and textField

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Box, Button, TextField } from "@mui/material";
 
 function App() {
-  console.log("swag");
+  const [cityInput, setCityInput] = useState("");
+  console.log(cityInput);
 
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=london,GB&limit=5&appid=0b03cdb17a8af366b8d2a6f9f57c6c6e"
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      const long = data[0].lon;
-      const lat = data[0].lat;
-      return [lat, long];
-    })
-    .then((data) =>
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${data[0]}&lon=${data[1]}&appid=0b03cdb17a8af366b8d2a6f9f57c6c6e`
-      )
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-    );
-
-  // fetch(
-  //   "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=24d97d22302d104b7ea6b85767ffa7e4"
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => console.log(data));
+  useEffect(() => {
+    fetch(
+      "http://api.openweathermap.org/geo/1.0/direct?q=london,GB&limit=5&appid=0b03cdb17a8af366b8d2a6f9f57c6c6e"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const long = data[0].lon;
+        const lat = data[0].lat;
+        return [lat, long];
+      })
+      .then((data) =>
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${data[0]}&lon=${data[1]}&appid=0b03cdb17a8af366b8d2a6f9f57c6c6e`
+        )
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+      );
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      height={"100vh"}
+      display="flex"
+      justifyContent="center"
+      backgroundColor="dimgray"
+    >
+      <Box display="flex" flexDirection="column" marginTop={10}>
+        <TextField
+          id="region"
+          label="Enter City"
+          variant="filled"
+          value={cityInput}
+          onChange={(e) => setCityInput(e.target.value)}
+        />
+        <Button variant="contained">Get Weather</Button>
+      </Box>
+    </Box>
   );
 }
 
